fd31a8e1c3b816081add634e32c9f55535b2d69d
byBlurr <dbldumont11@gmail.com>
2017-12-24 02:53:25 +0000

    Update version

--- BruzeBotV2/util/References.cs
+++ BruzeBotV2/util/References.cs
@@ -7,7 +7,7 @@ namespace BruzeBotV2.util
     class References
     {
         public static string TITLE = "BRUZE V2";
-        public static string ID = "v2.0.4";
+        public static string ID = "v2.0.5";
         public static string DEV = "Blurr Development";
         public static string DEVWEB = "www.blurrdev.com";
     }
